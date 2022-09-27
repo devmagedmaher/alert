3c923081ad402022fe76141fe9e051809da5171d
Maged Maher <dev.magedmaher@gmail.com>
2022-09-27 20:12:13 +0200

    finish join a room page

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,20 +1,94 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@mantine/core';
+import {
+  Button,
+  Input,
+} from '@mantine/core';
 
 const JoinPage = () => {
   const navigate = useNavigate()
-  const [room, setRoom] = React.useState('')
-  const [name, setName] = React.useState('')
+  const [values, setValues] = React.useState({
+    room: '',
+    name: '',
+  })
+  const [errors, setErrors] = React.useState({})
+
+  const validator = () => {
+    const e = {}
+
+    if (values.room?.trim() === '') {
+      e.room = 'Please enter the room code!'
+    }
+
+    if (values.name?.trim() === '') {
+      e.name = 'Please enter your name!'
+    }
+
+    setErrors(e)
+
+    return Object.keys(e).length > 0
+      ? false
+      : true
+  }
 
   const goToRoom = () => {
-    navigate(`/r/${room}`)    
+    if (validator()) {
+      // clear errors
+      setErrors({})
+      // go to room page
+      navigate(`/r/${values.room}`, {
+        state: {
+          name: values.name,
+        },
+      })
+    }
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target
+    console.log({ name, value })
+    setValues(v => ({ ...v, [name]: value }))
+    setErrors(e => ({ ...e, [name]: null}))
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Button mt={50} onClick={goToRoom}>Join</Button>
+        <Input.Wrapper
+          error={errors.room}
+          size="lg"
+          mb={10} mt={10}
+          >
+          <Input
+            name="room"
+            placeholder='Room Code'
+            value={values.room}
+            onChange={handleChange}
+            invalid={errors.room}
+            size='lg'
+          />
+        </Input.Wrapper>
+
+        <Input.Wrapper
+          error={errors.name}
+          size="lg"
+          mb={10} mt={10}
+          >
+          <Input
+            name="name"
+            placeholder='Your name'
+            value={values.name}
+            onChange={handleChange}
+            invalid={errors.name}
+            size='lg'
+          />
+        </Input.Wrapper>
+
+        <Button
+          onClick={goToRoom}
+          size='md'
+          mt={40}
+        >Join</Button>
       </header>
     </div>
   );
