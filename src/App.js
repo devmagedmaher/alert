6441264f5c14a8d1ea149f7b48b5a0ba37244eac
Maged Maher <dev.magedmaher@gmail.com>
2022-09-30 15:02:46 +0200

    fix home styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css';
 import Router from './router'
 import { MantineProvider } from '@mantine/core';
+import theme from './theme';
 
 function App() {
   return (
     <React.StrictMode>
-      <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
         <Router />
       </MantineProvider>
     </React.StrictMode>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import logo from '../assets/images/logo.png';
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@mantine/core';
+import { Box, Button, Center, Container, createStyles, Image, Stack } from '@mantine/core';
+
+const useStyles = createStyles(theme => ({
+  logo: {
+    width: 250,
+
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      width: '50vw',
+      maxWidth: 250
+    }
+  }
+}))
 
 const HomePage = () => {
+  const { classes } = useStyles()
   const navigate = useNavigate()
 
   const goToJoinPage = () => {
@@ -11,14 +23,29 @@ const HomePage = () => {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className='vertical-animation'>
-          <img src={logo} className="App-logo horizontal-animation" alt="logo" />
-        </div>
-        <Button mt={50} onClick={goToJoinPage}>Join a room</Button>
-      </header>
-    </div>
+    <Container>
+      <Center style={{ height: '100vh' }}>
+        <Stack>
+          <Box className='vertical-animation'>
+            <Box className='horizontal-animation'>
+              <div className={classes.logo}>
+                <Image
+                  src={logo}
+                  radius={10}
+                  alt="logo"
+                />
+              </div>
+            </Box>
+          </Box>
+          <Button
+            onClick={goToJoinPage}
+            variant="subtle"
+            size="lg"
+            mt={40}
+          >Join a room {" ->"}</Button>
+        </Stack>
+      </Center>
+    </Container>
   );
 }
 
