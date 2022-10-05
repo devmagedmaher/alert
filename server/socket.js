d290b3493ff10b1047827e9bd1f96e2a8b0e7908
Maged Maher <dev.magedmaher@gmail.com>
2022-10-06 00:26:15 +0200

    re-establish messaging

--- server/socket.js
+++ server/socket.js
@@ -11,11 +11,23 @@ module.exports = io => {
       // create/get room by name
       const r = rooms.create(room, { refresh })
 
+      // check if there is a player connected with the same id
+      if (r.getPlayer(id)?.isConnected) {
+        selfMessage('You are already connected on different tab or window.', 'error')
+        socket.disconnect()
+        return
+      }
+
       // join player to room and get player instance
       const p = r.join(id, name)
 
       // join socket to room
       socket.join(room)
+
+      // send message to room about this player connection
+      selfMessage(`You have connected to room ${room} successfully`, 'success')
+      broadcastMessage(`${name} have connected to this room`)
+
 
       /**
        * refresh room object
@@ -52,7 +64,19 @@ module.exports = io => {
       }
       
       socket.on('enterGame', handleEnterGameEvent) 
-      socket.on('disconnect', handleDisconnectEvent) 
+      socket.on('disconnect', handleDisconnectEvent)
+
+      function selfMessage(text, type) {
+        socket.emit('message', { text, type })
+      }
+
+      function broadcastMessage(text, type) {
+        socket.broadcast.to(room).emit('message', { text, type })
+      }
+
+      function roomMessage(text, type) {
+        io.to(room).emit('message', { text, type })
+      }
     }
     catch(e) {
       console.log('[ERROR][SOCKET]', e);
